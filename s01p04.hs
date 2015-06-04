bf1ca794f897858f9008fb8740a1c3ef1997482b
Csaba Marosi <cs.marosi@gmail.com>
2015-06-04 18:10:01 +0000

    Making computations parallel.

--- s01p04.hs
+++ s01p04.hs
@@ -1,13 +1,16 @@
 module Main where
 import System.Environment
 import Set1Module
+import Control.Parallel.Strategies
 
 slow :: String -> (String, [Int])
 slow x = (x, getXorKey x)
 
+-- to test possible parallelizations
+fast :: String -> (String, [Int])
+fast x = (x, [0..255])
+
 main :: IO ()
 main = do
-    content <- readFile "4.txt"
-    let withKeys = map slow $ lines content
-        candidates = filter (\x -> 1 == (length . snd $ x)) withKeys
-    mapM_ (putStrLn . decryptXor) $ map fst candidates
+    content <- readFile "4.txt" --a 327 lines long file
+    print $ parMap rpar slow $ lines content
